d124ac749c7f92b10315682d3c16380dba62ed21
sthDINESH <sthapit.dnsh@gmail.com>
2025-10-16 21:55:37 +0100

    add feature to create bookings

--- booking/urls.py
+++ booking/urls.py
@@ -4,8 +4,16 @@ from . import views
 urlpatterns = [
     path('workspaces/', views.workspace_list, name='workspace_list'),
     path('booking/create/', views.create_booking, name='create_booking'),
-    path('booking/<int:booking_id>/update', views.update_booking, name='update_booking'),
-    path('booking/<int:booking_id>/cancel', views.cancel_booking, name='cancel_booking'),
+    path(
+        'booking/<int:booking_id>/update',
+        views.update_booking,
+        name='update_booking'
+    ),
+    path(
+        'booking/<int:booking_id>/cancel',
+        views.cancel_booking,
+        name='cancel_booking'
+    ),
     path('', views.home, name='home'),
     path('my-bookings/', views.my_bookings, name='my_bookings'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,84 +1,124 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .models import WorkSpace, Booking
-from .forms import BookingForm
+from .forms import BookingForm, CheckBookingsForm
 
 
 def home(request):
-    
+    """
+    """
+    if request.method == "POST":
+        check_bookings_form = CheckBookingsForm(request.POST)
+
+        if check_bookings_form.is_valid():
+            date = check_bookings_form.cleaned_data['date']
+            start_time = check_bookings_form.cleaned_data['start_time']
+            end_time = check_bookings_form.cleaned_data['end_time']
+
+            url = (
+                reverse('home')
+                + f"?date={date}&start_time={start_time}&end_time={end_time}"
+            )
+            return redirect(url)
+        else:
+            return redirect('home')
+
+    if (
+        request.GET.get('date')
+        and request.GET.get('start_time')
+        and request.GET.get('end_time')
+    ):
+        date = request.GET.get('date')
+        start_time = request.GET.get('start_time')
+        end_time = request.GET.get('end_time')
+
+    else:
+        now = datetime.now()
+        date = now.date().isoformat()
+        start_time = now.strftime('%H:%M')
+        end_time = (now + timedelta(hours=1)).strftime('%H:%M')
+
+    check_bookings_form = CheckBookingsForm(initial={
+        'date': date,
+        'start_time': start_time,
+        'end_time': end_time,
+    })
+
+    # Exclude workspaces with overlapping bookings
+    available_workspaces = WorkSpace.objects.exclude(
+        bookings__booking_date=date,
+        bookings__start_time__lt=end_time,
+        bookings__end_time__gt=start_time,
+    )
+
     booking_form = BookingForm()
 
-    bookings = Booking.objects.filter(
-        user=request.user,
-    )
+    workspaces = WorkSpace.objects.all()
 
     return render(
         request=request,
 
         context={
             "booking_form": booking_form,
-            "bookings": bookings,
+            "check_bookings_form": check_bookings_form,
+            "workspaces": workspaces,
+            "available_workspaces": available_workspaces,
         },
         # template_name="booking/home.html",
-        template_name="booking/interactive_floorplan.html", 
-
+        template_name="booking/interactive_floorplan.html",
+        # template_name="booking/workspace_list.html",
     )
 
 
 def workspace_list(request):
     workspaces = WorkSpace.objects.all()
-    return render(request, 'booking/workspace_list.html', {'workspaces': workspaces})
-
-# debugging line to check if workspaces are being fetched correctly
-# print(WorkSpace.objects.all())
+    return render(
+        request,
+        'booking/workspace_list.html',
+        {'workspaces': workspaces}
+    )
 
 
 @login_required
 def create_booking(request):
     """
     Create a new booking related to :model:`Booking`
-    Supports AJAX POST requests
-    Return JSON response
     """
     if request.method == "POST":
-        try:
-            booking_form = BookingForm(data=request.POST)
+        booking_form = BookingForm(data=request.POST)
 
-            if booking_form.is_valid():
-                booking = booking_form.save(commit=False)
-                booking.user = request.user
-                booking.status = "confirmed"
-                booking.save()
-                return JsonResponse({
-                    'success': True,
-                    'message': 'Booking created successfully.'
-                })
-            else:
-                return JsonResponse(
-                    {
-                        'success': False,
-                        'message': 'Errors in submitted booking form',
-                        'errors': booking_form.errors
-                    },
-                    status=400
-                )
-        except Exception as e:
-            return JsonResponse(
-                {
-                    'success': False,
-                    'message': f'Error: {str(e)}'
-                },
-                status=500
+        if booking_form.is_valid():
+            booking = booking_form.save(commit=False)
+            booking.user = request.user
+            booking.status = "confirmed"
+            booking.save()
+
+            date = booking_form.cleaned_data['booking_date']
+            start_time = booking_form.cleaned_data['start_time']
+            end_time = booking_form.cleaned_data['end_time']
+
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Booking confirmed",
             )
-    else:
-        return JsonResponse(
-            {
-                'success': False,
-                'message': 'Unsupported request method.'
-            },
-            status=405
-        )
+            url = (
+                reverse('home')
+                + f"?date={date}&start_time={start_time}&end_time={end_time}"
+            )
+            return redirect(url)
+        else:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "Booking could not be made. Select a different slot",
+            )
+    return redirect('home')
 
 
 @login_required
@@ -163,10 +203,8 @@ def cancel_booking(request, booking_id):
             status=500
         )
 
-    pass
 
 @login_required
 def my_bookings(request):
     bookings = Booking.objects.filter(user=request.user)
     return render(request, 'booking/my_bookings.html', {'bookings': bookings})
-
