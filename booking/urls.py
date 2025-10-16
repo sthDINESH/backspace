5eef914a06a7428ad8620f509b07d32f812c90a1
Christian <cxbeale@gmail.com>
2025-10-16 18:35:37 +0100

    add my-bookings page

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('booking/<int:booking_id>/update', views.update_booking, name='update_booking'),
     path('booking/<int:booking_id>/cancel', views.cancel_booking, name='cancel_booking'),
     path('', views.home, name='home'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
 ]
