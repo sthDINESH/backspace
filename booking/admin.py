97b0458e7591cbad4e799325959374e42111b5ec
Christian <cxbeale@gmail.com>
2025-10-16 13:00:50 +0100

    workspaces model, view, and template

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import WorkSpace
 
-# Register your models here.
+admin.site.register(WorkSpace)
--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('workspaces/', views.workspace_list, name='workspace_list'),
 ]
